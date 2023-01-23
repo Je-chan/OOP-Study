2d9e1a49c01259292a50fabacb889e9c941905a3
Je-chan <jkyyc3@gmail.com>
2023-01-23 15:08:58 +0900

    CREATE : 싱글톤 패턴

---
+++ src/ch18/CompanyTest.java
@@ -0,0 +1,17 @@
+package ch18;
+
+import java.util.Calendar;
+
+public class CompanyTest {
+    public static void main(String[] args) {
+
+        Company company1 = Company.getInstance();
+        Company company2 = Company.getInstance();
+
+        System.out.println(company1);
+        System.out.println(company2);
+
+        // 대표적으로 싱글톤 패턴을 사용하는 객체
+        Calendar calendar = Calendar.getInstance();
+    }
+}
